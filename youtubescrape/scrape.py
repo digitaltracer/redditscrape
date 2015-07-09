90dda779aae769a25c353b5be6a3a4b8dfd5df17
Adarsh N B <adarsh@zlemma.com>
2015-07-10 02:35:00 +0530

    rejecting playlists from the list

--- youtubescrape/scrape.py
+++ youtubescrape/scrape.py
@@ -3,7 +3,10 @@ Get Video links, title and date of video posted
 """
 
 import requests
+import logging
 from bs4 import BeautifulSoup
+
+LOGGING = logging.getLogger(__name__)
 
 
 class YoutubeScrape(object):
@@ -22,14 +25,16 @@ class YoutubeScrape(object):
     def get_video_divs(self):
         """ get all the video div tags from soup """
         self.video_divs = self.soup.findAll(
-            "li", {"class": "channels-content-item"})
+            "li", {"class": "channels-content-item"})[:15]
 
     def process_data(self):
         """ get all video infos in json from list of single video soups """
         if not self.video_divs:
             self.get_video_divs()
         for video_soup in self.video_divs:
-            self.video_infos.append(self.get_video_info(video_soup))
+            video_info = self.get_video_info(video_soup)
+            if video_info:
+                self.video_infos.append(video_info)
 
     def get_video_info(self, single_video_soup):
         """ get video title and url from single video soup """
@@ -41,8 +46,12 @@ class YoutubeScrape(object):
         video_attrs = video_tag.attrs
         if not video_attrs.get("href"):
             return {}
+        watch_url = video_attrs.get("href", "")
+        if watch_url.startswith("/playlist"):
+            return {}
         video_url = self.youtube + video_attrs.get("href", "")
         title = video_attrs.get("title", "")
+        LOGGING.debug("Getting info of video " + video_url)
         video_html = requests.get(video_url).content.strip()
         video_soup = BeautifulSoup(video_html)
         published_text = video_soup.find("strong",
